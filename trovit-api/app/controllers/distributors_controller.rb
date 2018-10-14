b68dd423fee69c069e059f665e51930a2e0465a2
Jose Ramirez <jmramirezr@unal.edu.co>
2018-10-14 09:38:40 -0500

    routes configuration and authentication

--- trovit-api/app/controllers/distributors_controller.rb
+++ trovit-api/app/controllers/distributors_controller.rb
@@ -13,21 +13,6 @@ class DistributorsController < ApplicationController
         render json: distributor, status: 200
     end
 
-    def create
-
-        user = User.find(params[:user_id])
-        distributor = Distributor.new(user_id: user.id)
-
-           if distributor.save
-                respond_to do |format|
-                    format.json {render json: distributor, status: 201}
-                end
-            else
-                respond_to do |format|
-                    format.json {render json: distributor.errors, status: :unprocessable_entity}
-                end 
-            end
-    end
 
     def destroy
         distributor = Distributor.find(params[:id])
