e444d453e2182de8e57093a85a37bd5d46b00767
Jose Ramirez <jmramirezr@unal.edu.co>
2018-10-16 15:31:52 -0500

    serializers feature completed

--- trovit-api/app/serializers/business_manager_serializer.rb
+++ trovit-api/app/serializers/business_manager_serializer.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: business_managers
+#
+#  id         :integer          not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :integer
+#  company_id :integer
+#
+
 class BusinessManagerSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :user_id
 end
