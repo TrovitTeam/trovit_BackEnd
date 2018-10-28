db91362f2b09eaa2b4c6db825ff189edfc934920
Jose Ramirez <jmramirezr@unal.edu.co>
2018-10-28 10:43:00 -0500

    deleted token for sign fb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181028030536) do
+ActiveRecord::Schema.define(version: 20181026023904) do
 
   create_table "business_managers", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -112,7 +112,6 @@ ActiveRecord::Schema.define(version: 20181028030536) do
     t.datetime "created_at",                              null: false
     t.datetime "updated_at",                              null: false
     t.string   "password_digest"
-    t.string   "token"
   end
 
 end
