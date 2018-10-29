956f646f6d81635ce3eb8fb7914ceaeecf36ba0b
Jose Ramirez <jmramirezr@unal.edu.co>
2018-10-29 01:14:18 -0500

    production changes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,22 +26,6 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = true
-
-  config.action_mailer.smtp_settings = {
-  address:              'smtp.gmail.com',
-  port:                 587,
-  domain:               'gmail.com',
-  user_name:            'trovitapi@gmail.com',
-  password:             'ingesoft2018',
-  authentication:       :plain,
-  enable_starttls_auto: true
-
-}
-
-  config.action_mailer.perform_caching = false
-
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,5 +88,6 @@ Rails.application.configure do
   authentication:       :plain,
   enable_starttls_auto: true
 
-}
+  }
+  config.action_mailer.perform_caching = false
 end
